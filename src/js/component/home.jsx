f1fd208b42a09253d99f1be7f657897a9a72efa6
Marione Fiszman <marionef1985@gmail.com>
2023-03-10 14:53:35 +0000

    structure and basic functions

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,58 @@
-import React from "react";
+import React, { useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
+
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+  const [taskState, setTaskState] = useState([]);
+  const theTaskList = [
+    <ul>
+      <li>hola</li>
+      <li>tu</li>
+	  <li>tu</li>
+	  <li>tu</li>
+    </ul>,
+  ];
+
+  const addTask = () => {
+    let newTask = document.getElementById("list").value;
+    theTaskList.push(newTask);
+    setTaskState(theTaskList);
+    return false;
+  };
+
+  /*const addTask = () => {
+
+	<form id="CreateForm" action="/create" method="post">
+    <input type="text" name="foo" />
+</form>
+
+		let task = document.getElementById("#task");
+		
+}*/
+
+  return (
+    <div className="text-center">
+      <h1 className="text-center mt-5">todos</h1>
+      <form id="taskListForm">
+        <label>
+          <input type="text" name="name" />
+        </label>
+      </form>
+      <div id="list">{taskState}</div>
+      <button
+        type="submit"
+        //form="taskListForm"
+        onClick={() => {
+          addTask();
+        }}
+      >
+        Add
+      </button>
+    </div>
+  );
 };
 
 export default Home;
