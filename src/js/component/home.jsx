c428f224a214923c37445552c86ea688e609f734
Marione Fiszman <marionef1985@gmail.com>
2023-03-17 08:44:53 +0000

    first try

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,53 +1,31 @@
 import React, { useState } from "react";
 
 //include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 
 const Home = () => {
-  const [inputValue, setInputValue] = useState("");
-  const [todos, setTodos] = useState([]);
+  const [state, setState] = useState(" ");
+
+  async function updateState(evento) {
+    const httpResponse = await fetch(
+      "https://jsonplaceholder.typicode.com/todos",
+      {
+        method: "GET",
+      }
+    );
+    const postList = await httpResponse.json();
+    setState(postList);
+  }
+
   return (
     <div className="container">
       <h1>My Todos</h1>
-      <ul>
-        <li>
-          <input
-            type="text"
-            onChange={(e) => {
-              setInputValue(e.target.value);
-            }}
-            value={inputValue}
-            onKeyDown={(e) => {
-              console.log("key was pressed", e.key);
-              if (e.key === "Enter") {
-                console.log("Enter was pressed");
-                setTodos(todos.concat(inputValue));
-                setInputValue("");
-              }
-            }}
-            placeholder="What do you need to do?"
-          ></input>
-        </li>
-        {todos.map((item, index) => (
-          <li>
-            <div className="listItems">{item}</div>{" "}
-            <button
-              onClick={() => {
-                setTodos(
-                  todos.filter((t, currentIndex) => {
-                    index !== currentIndex
-                  })
-                );
-              }}
-            >
-              <i className="fa-solid fa-xmark"></i>
-            </button>
-          </li>
-        ))}
-      </ul>
-      <div>{todos.length} task</div>
+      {state.map(post=> (
+        <p>{post.title}</p>
+      ))}
+      <button onClick={(evento) => updateState(evento)}>Click Me!</button>
+      <div>{state.length} task</div>
     </div>
   );
 };
